c6869f3a0713c9d950b71f8159a6d7414228d153
arjun1098 <arjun.aravind1998@gmail.com>
2018-02-12 23:26:23 +0530

    Added the files necessary for the portal. Probably first and last commit.

---
+++ reportGen.php
@@ -0,0 +1,137 @@
+<?php
+require_once 'dompdf/lib/html5lib/Parser.php';
+require_once 'dompdf/lib/php-font-lib/src/FontLib/Autoloader.php';
+require_once 'dompdf/lib/php-svg-lib/src/autoload.php';
+require_once 'dompdf/src/Autoloader.php';
+Dompdf\Autoloader::register();
+
+use Dompdf\Dompdf;
+
+function setConn(){
+	$servername="localhost";
+	$user="root";
+	$pass="";
+	$dbname="MOCKS";
+
+    $CONN=new PDO("mysql:host=$servername;dbname=$dbname",$user,$pass);
+    $CONN->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+	return $CONN;
+}
+
+// instantiate and use the dompdf class
+$dompdf = new Dompdf();
+
+$htmlStart="<!DOCTYPE html>
+	<head>
+		<title>Report Portal</title>
+		<style>
+			h1, h2{
+				text-align: center;
+			}
+			ul{
+				width: 80%;
+				margin: 2% auto;
+			}
+			p{
+				width: 80%;
+				margin: 2% auto;
+				text-align: center;
+				font-size: 125%;
+				display: block;
+			}
+			
+			.info-titles{
+				width: 70%;
+				margin: 2% auto;
+			}
+			.marks{
+				padding-top: 2%;
+				padding-bottom: 2%;
+				width: 70%;
+				margin: 0% auto;
+				display: block;
+				text-align: center;
+				border-top: 1px solid black;
+				border-left: 1px solid black;
+				border-right: 1px solid black;
+			}
+			
+			.total{
+				width: 80%;
+				font-size: 115%;
+				margin: 2% auto 5.5% auto;
+			}
+		</style>
+	</head>
+	
+	<body>";
+	
+$content="";
+$reg=$_POST['numberInput'];
+
+
+try{
+    $conn=setConn();
+
+	$sql_stmt="SELECT* FROM SCORES WHERE REG_NO='$reg';";
+
+  	$stmt = $conn->query($sql_stmt);
+ 	$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+	
+	foreach($results as $row){
+		
+		$name=$row['NAME'];
+		$dept=$row['DEPT'];
+		$sec1=$row['SEC1'];
+		$sec2=$row['SEC2'];
+		$sec3=$row['SEC3'];
+		$sec4=$row['SEC4'];
+		$sec5=$row['SEC5'];
+		$total=$row['TOTAL'];
+		
+		$content.="
+				<h1>Mock Placements 2018</h1>
+				<h2>Aptitude Test Report Card</h2>
+				<br>
+				<ul>
+					<li>Scores of the candidate are shown per section. Each section is marked out of 5. The total score is out of 25.</li>
+					<li>Candidates must reproduce this report card on the day of the mock placements.</li>
+					<li>Candidates who fail to do so will not be permitted to attend the interviews.</li>
+				</ul>
+				<br>
+				<p class='info-titles'><b>$name</b></p>
+				<p class='info-titles'><b>$reg</b></p>
+				<p class='info-titles'><b>$dept</b></p>
+				<br>
+				<p class='marks'>
+					<b>Text Completion - I&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;-&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;$sec1/5</b>
+				</p>
+				<p class='marks'>
+					<b>Text Completion - II&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;-&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;$sec2/5</b>
+				</p>
+				<p class='marks'>
+					<b>Chart Interpretation - I&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;-&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;$sec3/5</b>
+				</p>
+				<p class='marks'>
+					<b>Chart Interpretation - II&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;-&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;$sec4/5</b>
+				</p>
+				<p class='marks' style='border-bottom: 1px solid black;'>
+					<b>Quantity Compare&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;-&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;$sec5/5</b>
+				</p>
+				<br>
+				<p class='total'><b>Your total score is $total out of 25.</b></p>";	
+		
+	}
+	
+} catch (PDOEXception $e){
+	echo $e."<br>";
+}
+		
+$htmlEnd="</body></html>";
+
+$html=$htmlStart.$content.$htmlEnd;
+
+$dompdf->loadHtml($html);
+$dompdf->render();
+$dompdf->stream();
+?>
