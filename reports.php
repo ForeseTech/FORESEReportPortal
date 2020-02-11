e39cdc784780a5bf47f18d527e76399014c517d2
Arjun Aravind <arjun.aravind1998@gmail.com>
2020-02-11 15:18:56 +0000

    Update appearance of reports page.

--- reports.php
+++ reports.php
@@ -1,70 +1,10 @@
 <?php
 
-	require 'sql_connections.php';
+	require("src/sql-connections.php");
+	require("src/calculations.php");
 
 	$regnumber=$_POST['regnumber'];
-	$name = "";
-	$dept = "";
-	$one= "";
-	$two = "";
-	$three = "";
-	$four = "";
-	$five = "";
-	$tech = "";
-	$percentile = "";
-
-	try{
-
-		$conn = getConn();
-		
-		//Our sql query which gets both the non-technical and technical questions.
-		$sql_query = "SELECT IF(COUNT(*)=1, 'YES', 'NO') AS IF_EXISTS FROM USERS WHERE REGNUMBER='$regnumber';";
-
-		$statement = $conn->query($sql_query);
-		$results = $statement->fetchAll(PDO::FETCH_ASSOC);	
-
-		//We generate each question along with the four options.
-		foreach($results as $row){
-	
-			if($row['IF_EXISTS']!="YES"){
-				echo "<script>alert('Your registration number does not exist in the database. Please note that you must have written the test and, if you are Option B, you must register with the Placement Cell first.');</script>";	
-				echo "<Script>window.location.href='index.html';</script>";
-			}
-
-		}
-
-	} catch (PDOException $e){
-		echo $e."<br>";
-	}
-
-	try{
-
-		$conn = getConn();
-		
-		//Our sql query which gets both the non-technical and technical questions.
-		$sql_query = " SELECT USERS.NAME AS NAME, USERS.DEPARTMENT AS DEPARTMENT, RESULTS.SECTION_ONE AS ONE, RESULTS.SECTION_TWO AS TWO, RESULTS.SECTION_THREE AS THREE, RESULTS.SECTION_FOUR AS FOUR, RESULTS.SECTION_FIVE AS FIVE, RESULTS.SECTION_TECH AS TECHNICAL, RESULTS.TOTAL AS TOTAL, (SELECT COUNT(USERS.REGNUMBER) FROM USERS, RESULTS WHERE USERS.REGNUMBER=RESULTS.REGNUMBER AND USERS.DEPARTMENT=(SELECT DEPARTMENT FROM USERS WHERE REGNUMBER='$regnumber') AND RESULTS.TOTAL < (SELECT TOTAL FROM RESULTS WHERE REGNUMBER='$regnumber'))/(SELECT COUNT(REGNUMBER) FROM USERS WHERE DEPARTMENT=(SELECT DEPARTMENT FROM USERS WHERE REGNUMBER='$regnumber'))*100 AS PERCENTILE FROM USERS, RESULTS WHERE RESULTS.REGNUMBER=USERS.REGNUMBER AND USERS.REGNUMBER='$regnumber'; ";
-
-		$statement = $conn->query($sql_query);
-		$results = $statement->fetchAll(PDO::FETCH_ASSOC);	
-
-		//We generate each question along with the four options.
-		foreach($results as $row){
-
-			$name = $row['NAME'];
-			$dept = $row['DEPARTMENT'];
-			$one = $row['ONE'];
-			$two = $row['TWO'];
-			$three = $row['THREE'];
-			$four = $row['FOUR'];
-			$five = $row['FIVE'];
-			$tech = $row['TECHNICAL'];
-			$percentile = $row['PERCENTILE'];
-
-		}
-
-	} catch (PDOException $e){
-		echo $e."<br>";
-	}
+	$student = get_student_information($regnumber);
 
 ?>
 <html>
@@ -90,52 +30,27 @@
 			<table>
 				<tr>
 					<th>Section</th>
-					<th>Score (Out of 5)</th>
+					<th>Score (Out of 50)</th>
 				</tr>
 
 				<tr>
-					<td><b>Word Comparison</b></td>
-					<td><b><?php echo $one; ?></b></td>
-				</tr>
-
-				<tr>
-					<td><b>Sentence Completion</b></td>
-					<td><b><?php echo $two; ?></b></td>
-				</tr>
-
-				<tr>
-					<td><b>Vocabulary</b></td>
-					<td><b><?php echo $three; ?></b></td>
-				</tr>
-
-				<tr>
-					<td><b>Quantity Comparison</b></td>
-					<td><b><?php echo $four; ?></b></td>
-				</tr>
-
-				<tr>
-					<td><b>Quantitative</b></td>
-					<td><b><?php echo $five; ?></b></td>
-				</tr>
-
-				<tr>
-					<td><b>Technical (out of 25)</b></td>
+					<td><b>Your Score</b></td>
 					<td><b><?php echo $tech; ?></b></td>
 				</tr>
 			</table>
 
 			<br>
 	
-			<p>He/She has scored more than <strong><?php echo $percentile; ?>%</strong> of students in their department.</p>
+			<p>58 students have attempted the test in the department.<br>Arjun has scored more than <strong><?php echo $percentile; ?>%</strong> of students in their department.<br><br>The candidate was tested on questions from the quantitative, verbal and technical categories. Technical questions counted for 50% of the questions asked to the candidate.</p>
 
-			<br>
+			<br><br><br>
 
 			<strong>NOTE:</strong><br>
 
 			<ul style="text-align: left;">
-				<li>These results <strong><em>must</em></strong> be shown to your interviewer on the day of the Mock Placements.</li><br>
-				<li>We recommend you take a <strong>screenshot</strong> of this page and store it in your phone.</li><br>
-				<li>Without this report, you will <strong><em>not</em></strong> be allowed to attend your interview.</li><br>
+				<li>These results <strong><em>must</em></strong> be shown to your interviewer on the day of the Mock Placements.</li>
+				<li>We recommend you take a <strong>screenshot</strong> of this page and store it in your phone.</li>
+				<li>Without this report, you will <strong><em>not</em></strong> be allowed to attend your interview.</li>
 				<li>For more queries, visit <a href="http://www.forese.in/students"> the official FORESE website.</a></li>
 			</ul>
 
